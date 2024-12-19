688a17c99941d6beda93bc3e948635c06ef58a0c
biboking-373 <vinnikinyua7@gmail.com>
2024-12-19 14:57:55 +0300

    navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,36 @@
-<script setup>
-import HelloWorld from './components/Auth/Authorization.vue';
+<script>
+export default {
+  name: 'App'
+}
 </script>
 
 <template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <router-link to="/" class="navbar-brand">
+          <div class="container d-flex justify-content-center align-items-center">
+          <img src="/BIG BITEZZZ.png" alt="Home page image"
+          class="rounded-circle overflow-hidden" style="width:90px; height:90px">
+          </div>
+        </router-link>
+        <div class="navbar-nav">
+          <router-link to="/" class="nav-item nav-link">Home</router-link>
+          <router-link to="/tutorials" class="nav-item nav-link">Tutorials</router-link>
+          <router-link to="/login" class="nav-item nav-link">Login</router-link>
+          <router-link to="/register" class="nav-item nav-link">Register</router-link>
+        </div>
+      </div>
+    </nav>
 
-  <HelloWorld/>
+    <div class="container mt-4">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+#app {
+  font-family: Arial, sans-serif;
+}
 </style>
